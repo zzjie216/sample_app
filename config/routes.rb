4e6aad58de9b7a4ffeec7bfbc4ffbb4f398c2016
zzjie216 <godwithzhongjie@163.com>
2013-10-22 12:04:25 +0800

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ SampleApp::Application.routes.draw do
   # get "static_pages/contact"
   # match '/', to: 'static_pages#home', via: 'get'
   resources :users
+  resources :sessions, only:[:new,:create,:destroy]
   root 'static_pages#home'
-  match '/signup',  to: 'users#new',             via: 'get'
+  match '/signup',   to: 'users#new',             via: 'get'
+  match '/signin',   to: 'sessions#new',          via: 'get'
+  match '/signout',  to: 'sessions#destroy',      via: 'delete'
   match '/help',     to: 'static_pages#help',     via: 'get'
   match '/about',    to: 'static_pages#about',    via: 'get'
   match '/contact',  to: 'static_pages#contact',  via: 'get'
